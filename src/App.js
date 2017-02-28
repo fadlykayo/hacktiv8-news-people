a0de6f8d1280278962748235c2d52c5a10203feb
Fadly Kayo <fadly.kayo@googlemail.com>
2017-02-28 17:45:14 +0700

    Done all release

--- src/App.js
+++ src/App.js
@@ -54,11 +54,47 @@ class Home extends Component {
   }
 }
 
-const About = () => (
-  <div>
-    <h5>About</h5>
-  </div>
-)
+class About extends Component {
+  constructor () {
+    super()
+    this.state = {
+      peoples: []
+    }
+  }
+
+  componentDidMount () {
+    this.fetchPeoples()
+  }
+
+  fetchPeoples () {
+    fetch('https://swapi.co/api/people/')
+      .then((response) => {
+        return response.json()
+      })
+      .then((resp) => {
+        this.setState({
+          peoples: resp.results
+        })
+      })
+  }
+
+  render () {
+    return (
+      <div>
+        <h5>Peoples</h5>
+        <ul>
+          {this.state.peoples.map((people, index) => {
+             return (
+               <li key={index}>
+                 {people.name}
+               </li>
+             )
+           })}
+        </ul>
+      </div>
+    )
+  }
+}
 
 const App = () => (
   <div className='App'>
