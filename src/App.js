fcbfc7e69b3fe471426dfa4a6afd071acf417f6d
Fadly Kayo <fadly.kayo@googlemail.com>
2017-03-01 12:27:51 +0700

    Update app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
-
 import logo from './logo.svg'
 import './App.css'
 
+import { BrowserRouter as Router } from 'react-router-dom'
 import { Menu } from './components'
 import { Routes } from './Routes'
 
